b89833e7cb5575a8369b7166712221107a69d981
ABastinou <101596380+BASTINOU777@users.noreply.github.com>
2022-10-03 20:50:12 +0200

    Front : modifs

--- frontend/frontend-app/src/pages/CreatPost.jsx
+++ frontend/frontend-app/src/pages/CreatPost.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-// import { useState } from 'react';
+import { useState, useRef } from 'react';
 // import { useForm } from "react-hook-form";
 import { createPost } from "../api/PostsAPI"
 import "../styles/Posts.css"
 
 
+
+
 function CreatePosts() 
 {
+  const [selectedFile, setSelectedFile] = useState(null);
   //au clic sur le bouton "poster", fonction de création de post et reload de la page
   function submitForm(event)
   { 
@@ -15,38 +18,25 @@ function CreatePosts()
       title: event.target.titre.value,
       content: event.target.content.value,
       author: localStorage.getItem("userName"),
-      attachment: event.target.attachment,
+      attachement: selectedFile
     }
-    createPost(data)
-    .then((response) => 
-    {
-      console.log(response);
-      alert("Le Post a bien été crée");
-      window.location.replace("/");
-    })
-  }
-  const customTxt = document.getElementById('customText');
-	const realFileBtn = document.getElementById('inputImage');
-
-	const clickRealButton = async () => {
-		const realFileBtn = document.getElementById('inputImage');
-		realFileBtn.click();
-	};
-  
-	const fileSelectedHandler = (event) => {
-		createPost({
-			selectedFile: event.target.files[0],
-		});
-		customTxt.innerHTML = realFileBtn.files[0].name;
-		console.log(realFileBtn.files[0].name);
-	};
+    console.log(data)
+    // createPost(data)
+    // .then((response) => 
+    // {
+    //   console.log(response);
+    //   alert("Le Post a bien été crée");
+    //   window.location.replace("/");
+    // })
+  } 
+   
     return (
       <main>
         <h1>Publier sur Groupomania</h1>
         <section>
           <article>
           <h2>Créer une publication</h2>
-            <form id="submitForm" onSubmit={submitForm}>
+            <form id="submitForm">
               <div>
                   <label htmlFor="Titre">Titre: </label><br/>
                   <input type="text" name="titre" placeholder='Titre'/>
@@ -56,21 +46,11 @@ function CreatePosts()
                   <input type="text" name="content" placeholder='Content'/>
                 </div>
                  <label>Ajouter une image :</label>
-				<div className="containerImageUpload">
-					<input
-						type="file"
-						onChange={fileSelectedHandler}
-						id="inputImage"
-						placeholder="Choisir un fichier"
-					/>
-					<button id="customButton" onClick={clickRealButton}>
-						<p>Ajouter une image</p>
-						<span className="material-icons">add_photo_alternate</span>
-					</button>
-					<div className="customText">
-						<span id="customText">Pas d'image ajoutée</span>
-					</div>
-				</div>
+                 <input
+          type="file"
+          value={selectedFile}
+          onChange={(e) => setSelectedFile(e.target.files[0])}
+        />
 				<button onClick={submitForm}> Publier</button>
               </form>
           </article>
